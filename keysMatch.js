b53022bf141ca278902a88ead1a88f990c790942
Linxue <angelren1220@gmail.com>
2023-11-09 01:01:31 -0500

    add keysMatch

--- keysMatch.js
+++ keysMatch.js
@@ -5,5 +5,23 @@
 // We can start with the following function definition:
 
 const keysMatch = function(obj1, obj2, keys) {
+  // loop all the key of keys
+  // check if both objs have the key
+  // if not return false
+  // check if key in obj1 === obj2
+  // if not return false
+  // return true
 
+  for(const key of keys) {
+    // if(!Object.keys(obj1).includes(key) || !Object.keys(obj2).includes(key)){
+    //   return false;
+    // }
+    if (!obj1.hasOwnProperty(key) || !obj2.hasOwnProperty(key)) {
+      return false;
+    }
+    if(obj1[key] !== obj2[key]){
+      return false
+    }
+  }
+  return true;
 };
