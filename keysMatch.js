241403c992e81d5d85b806a0cca9a8775b64dda5
Linxue <angelren1220@gmail.com>
2023-11-09 00:41:24 -0500

    add keysMatch question

---
+++ keysMatch.js
@@ -0,0 +1,9 @@
+// Build a function called keysMatch() which will be given two objects and an array of strings.
+
+// It will use the strings to look up the key-value pair in each object and compare the values. If all the values are explicitly equal to each other, return true, otherwise return false.
+
+// We can start with the following function definition:
+
+const keysMatch = function(obj1, obj2, keys) {
+
+};
