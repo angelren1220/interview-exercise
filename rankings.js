2bdb5e6c4661702c1ca450587a17c8a440eb5f9d
Linxue <angelren1220@gmail.com>
2023-11-09 01:06:28 -0500

    add rankings

---
+++ rankings.js
@@ -0,0 +1,19 @@
+// This and following questions will now be about writing logic using (pseudo) JavaScript on the "whiteboard".
+
+// My friend Lee is a fan of foosball (insert/use another 1-on-1 game here). Lee plays in an amateur foosball league, has asked us for help writing a program to understand player rankings.
+
+// We're given some game outcome data like the following:
+
+// [
+//   { winner: 'Alice', loser: 'Bob',   loser_points: 3 },
+//   { winner: 'Carol', loser: 'Dean',  loser_points: 1 },
+//   { winner: 'Elise', loser: 'Bob',   loser_points: 2 },
+//   { winner: 'Elise', loser: 'Carol', loser_points: 4 },
+//   { winner: 'Alice', loser: 'Carol', loser_points: 2 },
+//   { winner: 'Carol', loser: 'Dean',  loser_points: 3 },
+//   { winner: 'Dean',  loser: 'Elise', loser_points: 2 },
+// ]
+// Write a function that returns a list of all the player names (no player should be listed more than once).
+
+// ['Alice', 'Bob', 'Carol', 'Dean', 'Elise']
+
